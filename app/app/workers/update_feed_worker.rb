19f7a728447ceec8120efe4f769d84e33f493042
Stefan Trauth <mail@stefantrauth.de>
2014-03-21 09:01:50 +0100

    add some more rss elements to parse into the DB

--- app/app/workers/update_feed_worker.rb
+++ app/app/workers/update_feed_worker.rb
@@ -12,12 +12,20 @@ class UpdateFeedWorker
 				unless Episode.exists? :guid => episode.id
 					Episode.create!(
 						guid: episode.id,
+						podcast_id: podcast.id,
+
 						title: episode.title,
+						link: episode.url,
 						subtitle: episode.summary,
 						content: episode.content,
-						#pub_date: episode.published,
-						cached: false,
-						podcast_id: podcast.id
+						pub_date: episode.published,
+						#filesize_in_bytes: episode.filesize_in_bytes,
+						#duration: episode.duration,
+						flattr_url: episode.flattr_url,
+						#tags: episode.tags,
+						icon_url: episode.icon_url,
+						audio_file_url: episode.audio_file_url,
+						cached: false
 					)
 				end
 			end
--- app/config/initializers/feedjira_elements_init.rb
+++ app/config/initializers/feedjira_elements_init.rb
@@ -1,2 +1,13 @@
+# feed attributes
+
 Feedjira::Feed.add_common_feed_element('itunes:summary', :as => :itunes_summary)
 Feedjira::Feed.add_common_feed_element('itunes:image', :value => :href, :as => :logo_url)
+
+# item attributes
+
+#Feedjira::Feed.add_common_feed_entry_element('itunes:keywords', :as => :tags)
+Feedjira::Feed.add_common_feed_entry_element('itunes:image', :value => :href, :as => :icon_url)
+Feedjira::Feed.add_common_feed_entry_element('enclosure', :value => :url, :as => :audio_file_url)
+Feedjira::Feed.add_common_feed_entry_element('enclosure', :value => :length, :as => :filesize_in_bytes)
+Feedjira::Feed.add_common_feed_entry_element('itunes:duration', :as => :duration)
+Feedjira::Feed.add_common_feed_entry_element('atom:link', :value => :href, :as => :flattr_url)
