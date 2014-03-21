5591059a0b2965e9a61b25f63b141779bb2fdbb7
Stefan Trauth <mail@stefantrauth.de>
2014-03-21 17:14:21 +0100

    authenticate for sidekiq interface and dispplay login/logout links

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -17,7 +17,9 @@ App::Application.routes.draw do
   get '/episodes/:id/delete_cached_file', to: 'episodes#delete_cached_file'
   resources :episodes
 
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :admin do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
