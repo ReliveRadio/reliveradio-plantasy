10f1c93f3e51807626fad75534473fb9d687830a
Stefan Trauth <mail@stefantrauth.de>
2014-03-26 14:44:50 +0100

    add devise login macro

--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -1,4 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -44,6 +43,10 @@ RSpec.configure do |config|
 
   # call factory girl methods directly
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
+
   config.before(:each) do |example_method|
     # Clears out the jobs for tests using the fake testing
     Sidekiq::Worker.clear_all
