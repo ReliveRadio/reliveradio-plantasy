9fe8a01ac57feacc84d35850a82d039af4f0b1cf
Stefan Trauth <mail@stefantrauth.de>
2014-03-26 14:43:25 +0100

    include factory methods directly

--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # call factory girl methods directly
+  config.include FactoryGirl::Syntax::Methods
 end
