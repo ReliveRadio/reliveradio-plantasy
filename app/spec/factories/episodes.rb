d92e491e9d4df5761177de523ff652a101599a9c
Stefan Trauth <mail@stefantrauth.de>
2014-03-26 14:42:05 +0100

    create episodes factory

--- app/spec/factories/episodes.rb
+++ app/spec/factories/episodes.rb
@@ -1,19 +1,37 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :episode do
-    title "MyString"
-    link "MyString"
-    pub_date "2014-03-19 15:22:41"
-    guid "MyString"
-    subtitle "MyString"
-    content "MyText"
-    duration 1
-    flattr_url "MyString"
-    tags "MyString"
-    icon_url "MyString"
-    audio_file_url "MyString"
+    title Faker::Lorem.paragraph(1)
+    link Faker::Internet.url
+    pub_date Faker::Business.credit_card_expiry_date
+    guid Faker::Lorem.characters(10)
+    subtitle Faker::Lorem.paragraph(1)
+    content Faker::Lorem.paragraph(5)
+    duration Faker::Number.number(6)
+    flattr_url Faker::Internet.url
+    tags Faker::Lorem.paragraph(1)
+    icon_url Faker::Internet.url
+    audio_file_url Faker::Internet.url
     cached false
-    local_path "MyString"
+    local_path ""
+  end
+
+  factory :episode_cached do
+    title Faker::Lorem.paragraph(1)
+    link Faker::Internet.url
+    pub_date Faker::Business.credit_card_expiry_date
+    guid Faker::Lorem.characters(10)
+    subtitle Faker::Lorem.paragraph(1)
+    content Faker::Lorem.paragraph(5)
+    duration Faker::Number.number(6)
+    flattr_url Faker::Internet.url
+    tags Faker::Lorem.paragraph(1)
+    icon_url Faker::Internet.url
+    audio_file_url Faker::Internet.url
+    cached true
+    local_path Faker::Internet.url
   end
 end
