f0f7c1ea47a5c78899f0dc2700d252e2e73bec48
Stefan Trauth <mail@stefantrauth.de>
2014-04-08 17:56:15 +0200

    add endtime to playlist entry

--- app/db/migrate/20140322094236_create_playlist_entries.rb
+++ app/db/migrate/20140322094236_create_playlist_entries.rb
@@ -3,6 +3,7 @@ class CreatePlaylistEntries < ActiveRecord::Migration
     create_table :playlist_entries do |t|
       t.datetime :start_time
       t.boolean :premiere
+      t.datetime :end_time
 
       t.integer :channel_playlist_id
       t.integer :episode_id
