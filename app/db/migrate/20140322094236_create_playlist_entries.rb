3ca46cf1da12581b2ed379100c2f2ef3a17e1136
Stefan Trauth <mail@stefantrauth.de>
2014-04-08 17:56:26 +0200

    make premiere default to false in playlist entry

--- app/db/migrate/20140322094236_create_playlist_entries.rb
+++ app/db/migrate/20140322094236_create_playlist_entries.rb
@@ -2,8 +2,8 @@ class CreatePlaylistEntries < ActiveRecord::Migration
   def change
     create_table :playlist_entries do |t|
       t.datetime :start_time
-      t.boolean :premiere
       t.datetime :end_time
+      t.boolean :premiere, default: false
 
       t.integer :channel_playlist_id
       t.integer :episode_id
