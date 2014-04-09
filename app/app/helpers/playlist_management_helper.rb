7a3ae0c1c7e44971c5f876f79339279ad33d4e5c
Stefan Trauth <mail@stefantrauth.de>
2014-04-09 11:49:42 +0200

    helper is only for view

--- app/app/helpers/playlist_management_helper.rb
+++ app/app/helpers/playlist_management_helper.rb
@@ -1,5 +1,3 @@
-require 'ruby-mpd'
-
 module PlaylistManagementHelper
 	def first_x_words(str, n=20, finish=' ...')
 		if !str.blank?
@@ -11,28 +9,5 @@ module PlaylistManagementHelper
 		else
 			return ""
 		end
-	end
-
-	def update_mpd(channel_playlist)
-
-		# collect all future entries
-		playlist_entries = channel_playlist.playlist_entries.order(start_time: :desc)
-		# TODO do this in SQL!!!
-		playlist_entries.delete_if { |entry| entry.end_time < Time.now } # remove past entries
-		playlist_entries.delete_if { |entry| (entry.start_time < Time.now) && (entry.end_time > Time.now) } # remove live entry		
-
-		mpd = MPD.new
-		mpd.connect
-
-		# delete all mpd entries but not the currently playling one
-		length = mpd.queue.length - 1
-		mpd.delete 1..length if length > 0
-
-		# TODO add check if all episodes are cached!
-		
-		playlist_entries.each do |entry|
-			mpd.add File.basename(entry.episode.local_path)
-		end		
-		mpd.play
 	end
 end
