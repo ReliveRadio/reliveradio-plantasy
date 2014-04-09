af78b84b4cd5bcec9ebf6d54bda1931511ba24e0
Stefan Trauth <mail@stefantrauth.de>
2014-04-09 16:05:25 +0200

    validations for playlist entry

--- app/app/models/playlist_entry.rb
+++ app/app/models/playlist_entry.rb
@@ -1,6 +1,12 @@
 class PlaylistEntry < ActiveRecord::Base
 	belongs_to :episode
 	belongs_to :channel_playlist
+
+	validates :start_time, presence: true
+	validates :end_time, presence: true
+	validates :episode_id, presence: true
+	validates :channel_playlist_id, presence: true
+
 	def isLive?
 		((start_time < Time.now) && (end_time > Time.now))
 	end
