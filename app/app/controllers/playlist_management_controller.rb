b4173934cb8a8423c832183a912e80d301fd865d
Stefan Trauth <mail@stefantrauth.de>
2014-04-09 10:42:28 +0200

    first try on croping all episodes and adding new ones

--- app/app/controllers/playlist_management_controller.rb
+++ app/app/controllers/playlist_management_controller.rb
@@ -7,14 +7,12 @@ class PlaylistManagementController < ApplicationController
   	# TODO
   	# cut off all entries in the past?
   	@playlist_entries = @channel_playlist.playlist_entries.order(start_time: :asc)
+  	@playlist_entries.delete_if { |entry| entry.end_time < Time.now } # remove past entries
   end
 
   def create_entry
   	@episode = Episode.find(params[:episode_id])
   	@playlist_entries = @channel_playlist.playlist_entries.order(start_time: :asc)
-
-  	# TODO
-  	# cut off all entries in the past?
 
   	if @playlist_entries.blank?
   		start_time = Time.now
@@ -32,14 +30,33 @@ class PlaylistManagementController < ApplicationController
 	playlist_entries = @channel_playlist.playlist_entries.order(start_time: :asc)
 	# TODO do this in SQL!!!
 	playlist_entries.delete_if { |entry| entry.end_time < Time.now } # remove past entries
-	playlist_entries.delete_if { |entry| (entry.start_time < Time.now) && (entry.end_time > Time.now) } # remove live entry		
 
 	mpd = MPD.new
 	mpd.connect
 
 	# delete all mpd entries but not the currently playling one
-	length = mpd.queue.length - 1
-	mpd.delete 1..length if length > 0
+	status = mpd.status
+	if status[:playlistlength] > 0 && status[:state] == :play
+		# remove live entry
+		playlist_entries.delete_if { |entry| (entry.start_time < Time.now) && (entry.end_time > Time.now) }
+
+		# delete all played entries
+		if status[:song] > 0
+			mpd.delete 0...status[:song]
+		end
+		# update status because positions changed
+		status = mpd.status
+		# delete all future entries
+		start = status[:song] + 1
+		if start < status[:playlistlength]
+			mpd.delete start...status[:playlistlength]
+		end
+	else
+		mpd.clear if status[:state] == :stop
+		#TODO seek!!!
+	end
+
+	# if status is blank there is nothing in the queue
 
 	# TODO add check if all episodes are cached!
 	
@@ -47,6 +64,7 @@ class PlaylistManagementController < ApplicationController
 		mpd.add File.basename(entry.episode.local_path)
 	end		
 	mpd.play
+	mpd.disconnect
 
     respond_to do |format|
       format.html { redirect_to playlist_management_url, channel_playlist: @channel_playlist.id, notice: 'Episode was added to the playlist.' }
