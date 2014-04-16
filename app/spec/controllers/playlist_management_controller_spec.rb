f4c1c56dbebce2698898496bafbcc1c25f1c6162
Stefan Trauth <mail@stefantrauth.de>
2014-04-16 15:39:48 +0200

    fix basic index check

--- app/spec/controllers/playlist_management_controller_spec.rb
+++ app/spec/controllers/playlist_management_controller_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe PlaylistManagementController do
 
+  login_admin	
+
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      channel_playlist = create(:channel_playlist)
+      get 'index', channel_playlist: channel_playlist.id
       response.should be_success
     end
   end
