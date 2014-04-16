cd780be5d5995a7279ec58e021cadce61028a0a1
Stefan Trauth <mail@stefantrauth.de>
2014-04-16 15:24:24 +0200

    add search and pagination and sorting by column

--- app/app/controllers/podcasts_controller.rb
+++ app/app/controllers/podcasts_controller.rb
@@ -15,7 +15,13 @@ class PodcastsController < ApplicationController
   # GET /podcasts/1
   # GET /podcasts/1.json
   def show
-    @episodes = @podcast.episodes.order(:pub_date).reverse
+    @query = Episode.search(params[:q])
+    if params[:q]
+      @query.sorts = 'pub_date desc' if @query.sorts.empty?
+      @episodes = @query.result.paginate(:page => params[:page], :per_page => 15)
+    else
+      @episodes = @podcast.episodes.order(pub_date: :desc).paginate(:page => params[:page], :per_page => 15)
+    end
   end
 
   # GET /podcasts/new
