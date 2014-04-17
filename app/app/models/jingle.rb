25e4341265cedd618cbfe5d530a668af52dbaba3
Stefan Trauth <mail@stefantrauth.de>
2014-04-17 18:55:08 +0200

    add validation for jingle

--- app/app/models/jingle.rb
+++ app/app/models/jingle.rb
@@ -1,5 +1,10 @@
 class Jingle < ActiveRecord::Base
 	has_many :playlist_entries
+
+	validates :title, presence: true
+	validates :audio, presence: true
+	validates :duration, presence: true
+
 	mount_uploader :audio, AudioUploader
 	before_destroy :remove_uploaded_audio
 	
