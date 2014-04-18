e450cfd754c1d56e4cded46d9658ce1110a20b1b
Stefan Trauth <mail@stefantrauth.de>
2014-04-18 14:03:16 +0200

    use correct ajax url in schedule view

--- app/app/assets/javascripts/schedule.js
+++ app/app/assets/javascripts/schedule.js
@@ -3,6 +3,6 @@ $(function() {
 	var intervalTime = 10 * 1000; // 10 seconds
 	// start timer
 	window.setInterval(function(){
-		$.get($('#playlist-entry-list').data('fetch-url'), null, null, 'script');
+		$.get($('#episodes').data('update-url'), null, null, 'script');
 	}, intervalTime);
 });
