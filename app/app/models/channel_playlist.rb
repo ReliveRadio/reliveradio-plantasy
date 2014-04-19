f6375b7383b227d39c0f2812eb62a259e57f95fd
Stefan Trauth <mail@stefantrauth.de>
2014-04-19 13:36:45 +0200

    validate channel playlist

--- app/app/models/channel_playlist.rb
+++ app/app/models/channel_playlist.rb
@@ -1,3 +1,10 @@
 class ChannelPlaylist < ActiveRecord::Base
 	has_many :playlist_entries
+
+    validates :author, presence: true
+    validates :name, presence: true
+    validates :description, presence: true
+	validates :language, presence: true
+    validates :mpd_socket_path, presence: true
+
 end
