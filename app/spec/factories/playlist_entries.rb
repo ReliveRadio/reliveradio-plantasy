84063c184a8a73c8ce17004974fa17e6775977fc
Stefan Trauth <mail@stefantrauth.de>
2014-04-21 17:07:20 +0200

    fix playlist entry factory

--- app/spec/factories/playlist_entries.rb
+++ app/spec/factories/playlist_entries.rb
@@ -6,16 +6,22 @@ FactoryGirl.define do
   factory :playlist_entry_episode, class: PlaylistEntry do
     start_time Faker::Business.credit_card_expiry_date
     episode
-    end_time {start_time + episode.duration}
     premiere false
     channel_playlist
+
+    before(:create) do |entry|
+      entry.end_time = entry.start_time + entry.episode.duration
+    end
   end
 
   factory :playlist_entry_jingle, class: PlaylistEntry do
     start_time Faker::Business.credit_card_expiry_date
     jingle
-    end_time {start_time + jingle.duration}
     premiere false
     channel_playlist
+
+    before(:create) do |entry|
+      entry.end_time = entry.start_time + entry.jingle.duration
+    end
   end
 end
