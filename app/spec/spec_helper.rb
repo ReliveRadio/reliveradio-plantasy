03a2f8faf7ff998dd7b0244064c39c30da26eb90
Stefan Trauth <mail@stefantrauth.de>
2014-04-29 19:19:06 +0200

    add database cleaner to keep test database clean

--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -83,4 +83,18 @@ RSpec.configure do |config|
     end 
   end
 
+  # keep database clean
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
