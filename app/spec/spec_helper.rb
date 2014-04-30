0fd54c13efc03b9b5ff369bd649670d01ba5dcb0
Stefan Trauth <mail@stefantrauth.de>
2014-04-30 09:24:30 +0200

    fix database cleaner problems

--- app/spec/spec_helper.rb
+++ app/spec/spec_helper.rb
@@ -28,7 +28,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -87,8 +87,15 @@ RSpec.configure do |config|
 
   # keep database clean
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
