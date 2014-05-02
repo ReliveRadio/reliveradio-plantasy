edcb21216c2f2fcf8aaa7b52a69a6eda23808d6d
Stefan Trauth <mail@stefantrauth.de>
2014-05-02 18:01:14 +0200

    change audio file path back to non-public folder

--- app/app/uploaders/audio_uploader.rb
+++ app/app/uploaders/audio_uploader.rb
@@ -16,8 +16,8 @@ class AudioUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #Rails.root.to_s + "/audio/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Rails.root.to_s + "/audio/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
