eccd201f0f67821ffd255d0d9fe5b955663fc59f
Stefan Trauth <mail@stefantrauth.de>
2014-05-16 10:07:50 +0200

    support paged feeds

--- app/config/initializers/feedjira_init.rb
+++ app/config/initializers/feedjira_init.rb
@@ -1,5 +1,9 @@
-# feed attributes
+# paged feeds
+Feedjira::Feed.add_common_feed_element('atom:link', :value => :href, with: { rel: 'next' }, :as => :next_link)
+Feedjira::Feed.add_common_feed_element('atom:link', :value => :href, with: { rel: 'first' }, :as => :first_link)
+Feedjira::Feed.add_common_feed_element('atom:link', :value => :href, with: { rel: 'last' }, :as => :last_link)
 
+# feed attributes
 Feedjira::Feed.add_common_feed_element('itunes:summary', :as => :itunes_summary)
 Feedjira::Feed.add_common_feed_element('itunes:image', :value => :href, :as => :logo_url)
 Feedjira::Feed.add_common_feed_element('itunes:keywords', :as => :itunes_keywords)
@@ -17,4 +21,4 @@ Feedjira::Feed.add_common_feed_entry_element('itunes:image', :value => :href, :a
 Feedjira::Feed.add_common_feed_entry_element('enclosure', :value => :url, :as => :enclosure_url)
 Feedjira::Feed.add_common_feed_entry_element('enclosure', :value => :length, :as => :enclosure_length)
 Feedjira::Feed.add_common_feed_entry_element('itunes:duration', :as => :itunes_duration)
-Feedjira::Feed.add_common_feed_entry_element('atom:link', :value => :href, :as => :flattr_url)
+Feedjira::Feed.add_common_feed_entry_element('atom:link', :value => :href, with: { rel: 'payment' }, :as => :flattr_url)
