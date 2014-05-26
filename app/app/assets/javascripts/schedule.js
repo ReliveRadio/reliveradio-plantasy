09363a019f307fc2913045c25ec2320243452cf2
Stefan Trauth <mail@stefantrauth.de>
2014-05-26 10:23:59 +0200

    use one single toggle button to play and pause the stream

--- app/app/assets/javascripts/schedule.js
+++ app/app/assets/javascripts/schedule.js
@@ -1,12 +1,6 @@
 $(function() {
 
-	var player = document.getElementById("stream");
-	$('#startStream').click(function(){
-		player.play();
-	});
-	$('#pauseStream').click(function(){
-		player.pause();
-	});
+	init_livestream_player();
 
 	// refresh time in milliseconds
 	var intervalTime = 10 * 1000; // 10 seconds
@@ -18,3 +12,45 @@ $(function() {
 		}
 	}, intervalTime);
 });
+
+
+function init_livestream_player(){
+	var player = document.getElementById("stream");
+
+	// get all the available buttons
+	var livestreambuttons = $(".livestreambutton");
+
+	// initialize the buttons
+	livestreambuttons.each(function() {
+		$(this).click(liveStreamButtonClicked);
+		$(this).html('<i class="fa fa-play"></i> Livestream starten');
+	});
+
+	var isLivestreamPlaying = false;
+
+	function liveStreamButtonClicked() {
+		if(isLivestreamPlaying) {
+			// the button was clicked to STOP the livestream
+			player.pause();
+			livestreambuttons.each(function() {
+				// change look and feel of the button
+				$(this).html('<i class="fa fa-play"></i> Livestream starten');
+				$(this).removeClass('playing');
+				$(this).addClass('paused');
+			});
+			// remember status
+			isLivestreamPlaying = false;
+		} else {
+			// the button was clicked to START the livestream
+			player.play();
+			livestreambuttons.each(function() {
+				// change look and feel of the button
+				$(this).html('<i class="fa fa-pause"></i> Livestream stoppen');
+				$(this).removeClass('paused');
+				$(this).addClass('playing');
+			});
+			// remember status
+			isLivestreamPlaying = true;
+		}
+	}
+};
